b438e00564d87b216a4d35dfa1c4df106198b35a
jamesncox <jamesncox@yahoo.com>
2020-07-03 16:24:23 -0500

    Update category model

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,7 +1,3 @@
 class Api::V1::CategoriesController < ApplicationController
-    belongs_to :user
-    has_many :items 
-
-    validates :name, presence: { message: "%{value} of store is missing"  }
-    validates :color, presence: { message: "%{value} of header not selected" }
+   
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+    belongs_to :user
+    has_many :items 
+
+    validates :name, presence: { message: "%{value} of category is missing"  }
+    validates :color, presence: { message: "%{value} of header not selected" }
 end
