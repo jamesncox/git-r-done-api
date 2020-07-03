06e1a6b05194a2abaca03773ac784de4ce881e77
jamesncox <jamesncox@yahoo.com>
2020-07-03 16:07:14 -0500

    Move controllers to Api::V1:: and update models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :categories
+    has_many :items, through: :categories 
+
+    has_secure_password
+    validates :username, presence: true, uniqueness: true
+    validates :password_confirmation, presence: true, on: :create
 end
