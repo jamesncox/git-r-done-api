b9ef140aca2feb25a3b9469f66819baeacd6ba8f
jamesncox <jamesncox@yahoo.com>
2020-07-03 16:32:08 -0500

    Add actions to categories_controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,3 +1,47 @@
 class Api::V1::CategoriesController < ApplicationController
-   
+    def index
+        @categories = Category.all 
+        render json: @stores, except: [:created_at, :updated_at], status: 200
+    end
+
+    def user_categories
+        @user = User.find_by(id: params[:id])
+        @categories = @user.categories 
+        render json: @stores, except: [:created_at, :updated_at], status: 200
+    end
+
+    def show
+        @category = Category.find_by(id: params[:id])
+        render json: @category, status: 200
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            render json: @category, include: :items, status: 200
+        else 
+            render json: { errors: @store.errors.full_messages}, status: 400
+        end 
+    end
+
+    def update 
+        @category = Category.find_by(id: params[:id])
+        if @category.update(category_params)
+            render json: @category, include: :items, status: 200
+        else 
+            render json: { errors: @store.errors.full_messages}, status: 400
+        end
+    end 
+
+    def destroy
+        @category = Category.find_by(id: params[:id])
+        @category.items.delete_all
+        @category.destroy
+        render json: @category, status: 200
+    end
+
+    private
+        def category_params
+            params.require(:store).permit(:name, :color, :user_id)
+        end 
 end
