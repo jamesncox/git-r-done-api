ca6f507a85791d5ef933a0a56d414c80c118f207
jamesncox <jamesncox@yahoo.com>
2020-07-04 09:15:13 -0500

    Add priority to todos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_03_215213) do
+ActiveRecord::Schema.define(version: 2020_07_04_141331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_07_03_215213) do
     t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "priority"
   end
 
   create_table "users", force: :cascade do |t|
