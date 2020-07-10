6d4eebdbb37299f13e89026d1ca2c177d675489a
jamesncox <jamesncox@yahoo.com>
2020-07-09 19:14:47 -0500

    Add unique validation for list, custom error message

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,6 @@ class List < ApplicationRecord
     belongs_to :user
     has_many :todos 
 
-    validates :category, presence: { message: "%{value} name of list is missing"  }
+    validates :category, presence: { message: "%{value} name of list is missing"  }, uniqueness: { message: "%{value} Is Already Created!" }
     validates :color, presence: { message: "%{value} of list category not selected" }
 end
