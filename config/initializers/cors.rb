078667a429200064de5e9fdd72fc0a30a9278b42
jamesncox <jamesncox@yahoo.com>
2020-07-14 14:07:57 -0500

    Add do-did-done.netlify.app route to cros.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,15 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
   allow do
+    origins 'https://do-did-done.netlify.app'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+
+  allow do
     origins 'http://localhost:3001'
 
     resource '*',
